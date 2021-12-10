2ba07d59a75d057a819fcbdf37e6c68c9e79be13
arnani2212 <arnanplanco@gmail.com>
2021-12-10 11:38:15 +0800

    [PLANCO]Updated Frontend Dependencies

---
+++ client/src/utils/Theme.js
@@ -0,0 +1,18 @@
+export default {
+  colors: {
+    transparent: "transparent",
+    black: "#000",
+    white: "#fff",
+    gray: {
+      50: "#f7fafc",
+      700: "#2D3748",
+      900: "#171923",
+    },
+    company: {
+      background: "#212b3d",
+      violet: "#b075ff",
+      cyan: "#37b8ff",
+    },
+    // ...
+  },
+};
